918ec3b2155c7be1b3faf09e3d9f263f9bfaa431
Sai-prakash19 <120001007+Sai-prakash19@users.noreply.github.com>
2024-09-26 12:20:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
+import Hero from './components/hero/Hero'
 import Services from './components/Services/Services'
 import Title from './components/Title/Title'
 import About from './components/About/About'
